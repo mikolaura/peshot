a108baf1348df67337752a1b3a50894051c489b9
unknown <mikolaura58@gmail.com>
2023-07-15 19:30:49 +0300

    end design of profile screen

--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:travenor/views/widgets/mainButton.dart';
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
@@ -12,6 +14,19 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   File? _image;
+  TextEditingController _firstNameController = TextEditingController();
+  TextEditingController _secondNameController = TextEditingController();
+  TextEditingController _addressNameController = TextEditingController();
+  TextEditingController _phoneNameController = TextEditingController();
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _firstNameController.dispose();
+    _secondNameController.dispose();
+    _addressNameController.dispose();
+    _phoneNameController.dispose();
+  }
   void picImage()async{
     XFile? xFilePicked = await ImagePicker().pickImage(source: ImageSource.gallery);
     _image = File(xFilePicked!.path);
@@ -27,14 +42,66 @@ class _ProfileScreenState extends State<ProfileScreen> {
             onTap: picImage ,
             child: CircleAvatar(
               radius: 50,
-              backgroundColor: Colors.grey[
-                70
-              ],
+              backgroundColor: Colors.blueGrey,
             ),
-          )
-    
+          ),
+          const SizedBox(height: 45),
+          TextFC(controller: _firstNameController, label: 'First Name',)
+          ,const SizedBox(height: 15,),
+          TextFC(controller: _secondNameController, label: 'Second Name',)
+          ,const SizedBox(height: 15,),
+          TextFC(controller: _addressNameController, label: 'Address',)
+          ,const SizedBox(height: 15,),
+          TextFC(controller: _phoneNameController, label: 'Phone',isPhone: true,)
+          ,const SizedBox(height: 45,),
+          MainButton(text: 'Save Data', color: Theme.of(context).primaryColor)
         ],
       ),
+    );
+  }
+}
+
+class TextFC extends StatelessWidget {
+  TextEditingController controller;
+  String label;
+  bool isPhone;
+  TextFC({
+    Key? key,
+    required this.controller,
+    required this.label,
+    this.isPhone = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text (label),
+             SizedBox(
+              width:  (MediaQuery.of(context).size.width)* 0.5,
+              child: TextField(decoration: InputDecoration(
+                border: InputBorder.none,
+                
+              ),
+              textAlign: TextAlign.right,
+                keyboardType: isPhone ? TextInputType.phone : TextInputType.emailAddress,
+              ),
+            )
+
+          ],
+        ),
+        Container(
+          height: 1,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            
+          ),
+        )
+      ],
     );
   }
 }
